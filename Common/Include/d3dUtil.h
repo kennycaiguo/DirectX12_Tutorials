5d646329cb074cd832498a2e21abac3c746bf8d3
luchu <luchu1993@163.com>
2020-01-23 00:21:06 +0800

    #0 添加Shader编译帮助函数

--- Common/Include/d3dUtil.h
+++ Common/Include/d3dUtil.h
@@ -11,6 +11,7 @@
 #include <vector>
 #include <dxgi1_4.h>
 #include <d3d12.h>
+#include <d3dcompiler.h>
 #include <DirectXMath.h>
 #include <DirectXPackedVector.h>
 #include <DirectXColors.h>
@@ -125,4 +126,33 @@ inline Microsoft::WRL::ComPtr<ID3D12Resource> CreateDefaultBuffer(
 inline UINT CalcConstantBufferByteSize(UINT byteSize)
 {
 	return (byteSize + 255) & ~255;
+}
+
+inline Microsoft::WRL::ComPtr<ID3DBlob> CompileShader(
+	const std::wstring& filename,
+	const D3D_SHADER_MACRO* defines,
+	const std::string& entryPoint,
+	const std::string& target
+)
+{
+	UINT compileFlags = 0;
+#if defined _DEBUG || defined DEBUG
+	compileFlags = D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION;
+#endif
+	HRESULT hr = 0;
+	Microsoft::WRL::ComPtr<ID3DBlob> byteCode = nullptr;
+	Microsoft::WRL::ComPtr<ID3DBlob> errors = nullptr;
+
+	hr = D3DCompileFromFile(filename.c_str(), defines,
+		D3D_COMPILE_STANDARD_FILE_INCLUDE,
+		entryPoint.c_str(), target.c_str(),
+		compileFlags, 0, &byteCode, &errors);
+	if (errors != nullptr)
+	{
+		OutputDebugStringA((char*)errors->GetBufferPointer());
+	}
+
+	ThrowIfFailed(hr);
+	
+	return byteCode;
 }
